eace9397fe17d08d7431b6b8e8ff3137533837df
Aryawnnnn <aryanrajop18@gmail.com>
2026-04-07 08:53:39 +0530

    UC10: Count Total Seats in Train (reduce)

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -31,17 +31,11 @@ public class TrainConsistManagementApp {
         bogies.add(new Bogie("Sleeper", 72));
         bogies.add(new Bogie("AC Chair", 56));
         bogies.add(new Bogie("First Class", 24));
-        bogies.add(new Bogie("Sleeper", 70));
-        bogies.add(new Bogie("AC Chair", 58));
 
-        Map<String, List<Bogie>> groupedBogies = bogies.stream()
-                .collect(Collectors.groupingBy(Bogie::getName));
+        int totalCapacity = bogies.stream()
+                .map(b -> b.getCapacity())
+                .reduce(0, Integer::sum);
 
-        for (Map.Entry<String, List<Bogie>> entry : groupedBogies.entrySet()) {
-            System.out.println(entry.getKey() + ":");
-            for (Bogie b : entry.getValue()) {
-                System.out.println("  " + b);
-            }
-        }
+        System.out.println("Total seating capacity: " + totalCapacity);
     }
 }
